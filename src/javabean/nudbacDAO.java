8c6ba42a8336ae74f02083ad36ca68176a8eb471
Sun Pengyu <13122171080@163.com>
2015-12-26 19:56:53 +0800

    Revert "yes"
    This reverts commit 1f62871cefb4312740ffbd2ed91a83b0b7bc28e1.

--- src/javabean/nudbacDAO.java
+++
@@ -1,8 +0,0 @@
-package javabean;
-
-public interface nudbacDAO {
-	public boolean loginSuccess(String userName,String password);
-	public boolean signin(NormalUser auser);
-	public  void createMLtable(String mlname);
-		
-}
